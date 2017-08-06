a9aad84fae934c0726be18666575545fc037f67f
Guilherme Lourenço <guilhermefelipelourenco@hotmail.com>
2017-08-05 21:33:03 -0300

    initial commit

---
+++ vCardGenerator/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace vCardGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTelWork = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTelHome = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTelCel = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnDownloadVCard = new System.Windows.Forms.Button();
+            this.btnDownloadQr = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(87, 19);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(265, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(87, 45);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(265, 20);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last name:";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(87, 71);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(265, 20);
+            this.txtCompanyName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Company:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(87, 97);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(265, 20);
+            this.txtTitle.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Job Title:";
+            // 
+            // txtTelWork
+            // 
+            this.txtTelWork.Location = new System.Drawing.Point(87, 123);
+            this.txtTelWork.Name = "txtTelWork";
+            this.txtTelWork.Size = new System.Drawing.Size(265, 20);
+            this.txtTelWork.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Tel. Work:";
+            // 
+            // txtTelHome
+            // 
+            this.txtTelHome.Location = new System.Drawing.Point(87, 149);
+            this.txtTelHome.Name = "txtTelHome";
+            this.txtTelHome.Size = new System.Drawing.Size(265, 20);
+            this.txtTelHome.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Tel. Home:";
+            // 
+            // txtTelCel
+            // 
+            this.txtTelCel.Location = new System.Drawing.Point(87, 175);
+            this.txtTelCel.Name = "txtTelCel";
+            this.txtTelCel.Size = new System.Drawing.Size(265, 20);
+            this.txtTelCel.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Tel. Cell:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(87, 201);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(265, 20);
+            this.txtAddress.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(22, 204);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Address:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(87, 227);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(265, 20);
+            this.txtURL.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(22, 230);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "URL:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(87, 253);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(265, 20);
+            this.txtEmail.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(22, 256);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Email:";
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(358, 19);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(283, 283);
+            this.pbImage.TabIndex = 21;
+            this.pbImage.TabStop = false;
+            // 
+            // btnDownloadVCard
+            // 
+            this.btnDownloadVCard.Location = new System.Drawing.Point(25, 279);
+            this.btnDownloadVCard.Name = "btnDownloadVCard";
+            this.btnDownloadVCard.Size = new System.Drawing.Size(99, 23);
+            this.btnDownloadVCard.TabIndex = 22;
+            this.btnDownloadVCard.Text = "Download vCard";
+            this.btnDownloadVCard.UseVisualStyleBackColor = true;
+            this.btnDownloadVCard.Click += new System.EventHandler(this.btnDownloadVCard_Click);
+            // 
+            // btnDownloadQr
+            // 
+            this.btnDownloadQr.Location = new System.Drawing.Point(130, 279);
+            this.btnDownloadQr.Name = "btnDownloadQr";
+            this.btnDownloadQr.Size = new System.Drawing.Size(92, 23);
+            this.btnDownloadQr.TabIndex = 23;
+            this.btnDownloadQr.Text = "Download QR";
+            this.btnDownloadQr.UseVisualStyleBackColor = true;
+            this.btnDownloadQr.Click += new System.EventHandler(this.btnDownloadQr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 307);
+            this.Controls.Add(this.btnDownloadQr);
+            this.Controls.Add(this.btnDownloadVCard);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtTelCel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTelHome);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTelWork);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "vCard Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTelWork;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTelHome;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTelCel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnDownloadVCard;
+        private System.Windows.Forms.Button btnDownloadQr;
+    }
+}
+
---
+++ vCardGenerator/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using System.Drawing;
+using System.IO;
+namespace vCardGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownloadVCard_Click(object sender, EventArgs e)
+        {
+            string content = GenerateVCardCode();
+            SaveFileDialog svd = new SaveFileDialog();
+            svd.Title = "Save vCard";
+            svd.Filter = "VCard File (*.vcf)|.vcf";
+            svd.ShowDialog();
+
+            if (svd.FileName != "")
+            {
+                StreamWriter file = new StreamWriter(svd.FileName);
+                file.WriteLine(content);
+                file.Close();
+                MessageBox.Show("Arquivo salvo com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma pasta e infome o nome do arquivo");
+            }
+        }
+        private string GenerateVCardCode()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("BEGIN:VCARD");
+            sb.AppendLine("VERSION:2.1");
+            if (txtName.Text != "" && txtLastName.Text != "")
+            {
+                sb.AppendLine($"N;LANGUAGE=pt-br;CHARSET=Windows-1252:{txtLastName.Text};{txtName.Text}");
+                sb.AppendLine($"FN;CHARSET=Windows-1252:{txtName.Text} {txtLastName.Text}");
+            }
+            if(txtCompanyName.Text != "")
+                sb.AppendLine($"ORG:{txtCompanyName.Text}");
+            if (txtTitle.Text != "")
+                sb.AppendLine($"TITLE:{txtTitle.Text}");
+            if (txtTelWork.Text != "")
+                sb.AppendLine($"TEL;WORK;VOICE:{txtTelWork.Text}");
+            if (txtTelHome.Text != "")
+                sb.AppendLine($"TEL;HOME;VOICE:{txtTelHome.Text}");
+            if (txtTelCel.Text != "")
+                sb.AppendLine($"TEL;CELL;VOICE:{txtTelCel.Text}");
+            if (txtAddress.Text != "")
+                sb.AppendLine($"ADR;WORK;PREF:{txtAddress.Text}");
+            if (txtURL.Text != "")
+                sb.AppendLine($"URL;WORK:{txtURL.Text}");
+            if (txtEmail.Text != "")
+                sb.AppendLine($"EMAIL;PREF;INTERNET:{txtEmail.Text}");
+            sb.AppendLine("END:VCARD");
+            return sb.ToString();
+        }
+
+        private void btnDownloadQr_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                QRCodeEncoder qrEnc = new QRCodeEncoder();
+                qrEnc.QRCodeBackgroundColor = System.Drawing.Color.White;
+                qrEnc.QRCodeForegroundColor = System.Drawing.Color.Black;
+                //qrEnc.CharacterSet = "UTF-8";
+                qrEnc.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+                qrEnc.QRCodeScale = 3;
+                qrEnc.QRCodeVersion = 0;
+                qrEnc.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.Q;
+
+                Image img;
+                string data = GenerateVCardCode();
+                img = qrEnc.Encode(data);
+                pbImage.Image = img;
+
+                SaveFileDialog svd = new SaveFileDialog();
+                svd.Title = "Save QR Code";
+                svd.Filter = "Image files (*.jpg, *.jpeg, *.png)|*.jpg; *.jpeg; *.png";
+                svd.ShowDialog();
+
+                if(svd.FileName != "")
+                {
+                    img.Save(svd.FileName);
+                    MessageBox.Show("Arquivo salvo com sucesso!");
+                }
+                else
+                {
+                    MessageBox.Show("Selecione uma pasta e infome o nome do arquivo");
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro: {ex.Message}","QrCode Generator");
+            }
+        }
+    }
+}
